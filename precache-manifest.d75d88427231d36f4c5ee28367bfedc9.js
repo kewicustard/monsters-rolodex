b12c6262e1c5247bc0933503a47a4c98d4f4d753
kewicustard <kewicustard@gmail.com>
2020-04-01 10:40:41 +0700

    Updates

---
+++ precache-manifest.d75d88427231d36f4c5ee28367bfedc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0d9ecdba12f5f0646fcc79dcb81448",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "989e668f6b45d621d7ba",
+    "url": "/monsters-rolodex/static/css/main.f569addf.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989e668f6b45d621d7ba",
+    "url": "/monsters-rolodex/static/js/main.51226d75.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
